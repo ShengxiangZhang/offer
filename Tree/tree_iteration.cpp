ab2131fb79c5a2138dace412c612974407bcdd95
ShengxiangZhang <2275974639@qq.com>
2024-03-28 11:29:31 +0800

    modified:   .vscode/settings.json modified:   Tree/tree_iteration.cpp

--- Tree/tree_iteration.cpp
+++ Tree/tree_iteration.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
 #include<vector>
 #include<stack>
+#include<algorithm>
 
 using namespace std;
+
+// 定义树结构
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x): val(x), left(nullptr), right(nullptr){}
+};
 
 // 迭代法进行前序遍历
 
@@ -29,9 +38,34 @@ public:
 };*/
 
 // 迭代法进行中序遍历
-class Solution {
+/*class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> res;
+        TreeNode* cur = root;
+        while(cur!= nullptr || !st.empty()){
+            if(cur != nullptr){
+                st.push(cur);
+                cur = cur->left;
+            }
+            else{
+                cur = st.top();
+                res.push_back(cur->val);   
+                st.pop();
+                cur = cur->right;      
+            }
+        }
+        return res;
+    }
+};*/
+
+// 迭代法后序遍历
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root){
+        TreeNode* cur = root;
         stack<TreeNode*> st;
         vector<int> res;
         if(root == nullptr)
@@ -39,13 +73,14 @@ public:
         st.push(root);
         while(!st.empty()){
             TreeNode* cur = st.top();
-            res.push_back(cur->val);
             st.pop();
+            res.push_back(cur->val);
+            if(cur->left != nullptr)
+                st.push(cur->left);
             if(cur->right != nullptr)
                 st.push(cur->right);
-            if(cur->left != nullptr)
-                st.push(cur->left);        
         }
+        reverse(res.begin(), res.end()); // 将结果反转之后就是左右中的顺序了
         return res;
     }
-};
+}
