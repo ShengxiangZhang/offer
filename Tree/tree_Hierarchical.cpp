c20d63666f54b2f1d1bb801629408549107281e8
ShengXiangZhang <2275974639@qq.com>
2024-03-30 23:36:19 +0800

    modified:   Tree/tree_Hierarchical.cpp

--- Tree/tree_Hierarchical.cpp
+++ Tree/tree_Hierarchical.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 
 using namespace std;
 
@@ -11,11 +12,32 @@ struct TreeNode{
 };
 
 
+// 用队列解决层次遍历问题
+
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
+        queue<TreeNode*> que;
         vector<vector<int>> res;
-        TreeNode* cur;
-        while()
+        if(root == nullptr)
+            return res;
+        else
+            que.push(root);
+        while(!que.empty()){
+            vector<int> vec;
+            // 注意que.size()是不断变化的
+            int size = que.size();
+            for(int i = 0; i < size; i++){
+                TreeNode* temp = que.front();
+                que.pop();
+                vec.push_back(temp->val);
+                if(temp->left != nullptr)
+                    que.push(temp->left);
+                if(temp->right != nullptr)
+                    que.push(temp->right);
+            }
+            res.push_back(vec);
+        }
+        return res;
     }
 };
