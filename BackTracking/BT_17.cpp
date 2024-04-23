ee973f58ea53a9d85134527234da4ce1fce317e8
ShengXiangZhang <2275974639@qq.com>
2024-04-23 23:41:51 +0800

    modified:   BackTracking/BT_17.cpp

--- BackTracking/BT_17.cpp
+++ BackTracking/BT_17.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
@@ -8,7 +9,23 @@ class Solution {
 public:
     vector<string> res;
     string path;
+
+    void traversal(string& digits, int index, int len, vector<string>& map){
+        if(path.size() == len){
+            res.push_back(path);
+            return;
+        }
+        
+        for(int i = 0; i < map[digits[index] - 48].size(); i++){
+            path.push_back(map[digits[index] - 48][i]);
+            traversal(digits, index + 1, len, map);
+            path.pop_back();
+        }
+    }
+
     vector<string> letterCombinations(string digits) {
+        if(digits.size() == 0)
+            return res;
         vector<string> map;
         map.push_back("");
         map.push_back("");
@@ -20,7 +37,8 @@ public:
         map.push_back("pqrs");
         map.push_back("tuv");
         map.push_back("wxyz");
-        int len = digits.size()
-        if(path.size() == )
+        int len = digits.size();
+        traversal(digits, 0, len, map);
+        return res;
     }
 };
