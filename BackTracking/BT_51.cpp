51e1209f1baf1446b519694d961f223d7db6f2c4
ShengXiangZhang <2275974639@qq.com>
2024-04-29 16:16:47 +0800

    modified:   BackTracking/BT_51.cpp modified:   Greedy/Greedy_55.cpp new file:   Greedy/Greedy_55.exe

--- BackTracking/BT_51.cpp
+++ BackTracking/BT_51.cpp
@@ -4,10 +4,7 @@
 
 using namespace std;
 
-/***N皇后***/
 
-// 自己的想法，申请了一个二维的used数组
-/*
 class Solution {
 public:
     vector<vector<string>> res;
@@ -22,7 +19,7 @@ public:
         }
         return str;
     }
-    void backtracking(int n, int depth, vector<vector<bool>>& used){
+    void backtracking(int n, int depth, vector<vector<bool>> used){
         // 终止条件
         if(s.size() == n){
             res.push_back(s);
@@ -32,37 +29,26 @@ public:
         // 回溯
         for(int i = 0; i < n; i++){
             int flag = 0;
-            for(int j = depth-1; j >=0; j--){
+            for(int j = 0; j < depth; j++   ){
                 if(used[j][i] == true){
                     flag = 1;
                     break;
-                }    
-            }
-            int m = depth -1;
-            int k = i-1;
-            int j = i+1;
-            while(m >= 0){
-                if(k >= 0 && used[m][k] == true){
-                    flag = 1;
-                    break;
                 }
-                if(j < n && used[m][j] == true){
-                    flag = 1;
-                    break;
-                }
-                k--;
-                j++;
-                m--;
             }
-
             if(flag == 1)
                 continue;
+
+            if(depth-1 > 0){
+                if((i-1>0 && used[depth-1][i-1] == true))
+                    continue;
+                if((used[depth-1][i+1] == true))
+                    continue;
+            }
             string str = queen(i,n);
             s.push_back(str);
             used[depth][i] = true;
             backtracking(n, depth + 1, used);
             s.pop_back();
-            used[depth][i] = false;
 
         }
     }
@@ -76,4 +62,4 @@ public:
         backtracking(n, 0, used);
         return res;
     }
-};*/
+};
--- Greedy/Greedy_55.cpp
+++ Greedy/Greedy_55.cpp
@@ -6,7 +6,30 @@ using namespace std;
 
 class Solution {
 public:
+    // 局部最优：每次选取单步能到达最远距离的值，争取最大的跳跃步数
+    // 全局最优：得到整体能够到达的最远距离
     bool canJump(vector<int>& nums) {
-
+        for(int i = 0; i < nums.size(); i++){
+            int max = -1;
+            int index = -1;
+            for(int j = 1; j <= nums[i]; j++){
+                if(i + j + nums[i+j] >= nums.size() - 1)
+                    return true;
+                if( i + j + nums[i+j] > max){
+                    max = i + j + nums[i+j];
+                    index = i + j;
+                }   
+            }
+            i = index - 1;
+        }
+        return false;
     }
 };
+
+int main(){
+    vector<int> nums = {3,2,1,0,4};
+    Solution s;
+    bool b = s.canJump(nums);
+    system("pause");
+    return 0;
+}
