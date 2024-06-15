00b3371b2b3f47b7019ddd27ae052bc6ae7c0d61
ShengxiangZhang <2275974639@qq.com>
2024-06-15 17:47:18 +0800

    modified:   Graph/grid_827.cpp

--- Graph/grid_827.cpp
+++ Graph/grid_827.cpp
@@ -30,23 +30,23 @@ public:
 
     int largestIsland(vector<vector<int>>& grid) {
         int flag = 0;
-        int v = 1;
+        int v = 3;
         int res = 0;
         int maxS = 0;
         int m = grid.size();
         int n = grid[0].size();
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
+                v++;
                 if(grid[i][j] == 0){
                     flag = 1;
                     res = 0;
-                    v++;
                     for(int k = 0; k < 4; k++){
                         int nextx = i + dir[k][0];
                         int nexty = j + dir[k][1];
                         if(nextx >= m || nexty >= n || nextx < 0 || nexty < 0)
                                 continue;
-                        else if(grid[nextx][nexty] != 0 && grid[nextx][nexty] != v-1){
+                        else if(grid[nextx][nexty] != 0 && grid[nextx][nexty] != v){
                             cnt = 0;
                             dfs(grid,nextx,nexty,v);
                             res += cnt;
